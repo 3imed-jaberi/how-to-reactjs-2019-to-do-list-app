5cca145ae4172cd6e569edc1b371694b17616116
3imed-jaberi <imed_jebari@hotmail.fr>
2019-12-12 22:57:29 +0100

    add static data 🗳 ..

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+
+const FormTitle = 'To Do List App !';
+const BtnTitle = 'Add';
+const TechPlaceholder = 'Technologies .. ';
+const DescPlaceholder = 'Description .. ';
+
+
+
+export {
+  FormTitle,
+  BtnTitle,
+  TechPlaceholder,
+  DescPlaceholder
+};
