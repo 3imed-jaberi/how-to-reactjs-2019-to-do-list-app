93306c11de9fe430d26e1652126d27885ac71c5d
3imed-jaberi <imed_jebari@hotmail.fr>
2019-12-12 22:50:37 +0100

    display all UI components 📺 ..

--- src/index.js
+++ src/index.js
@@ -7,11 +7,30 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+// Our Components 
+import { CardItem, Input, TextArea, Button } from './components';
+
+
 
 class App extends React.Component {
   render(){
     return(
-      <h1> Welcome to ReactJS ! </h1>
+      <div className="shadow container-sm bg-danger p-5 mt-5">
+
+        <h2 className="text-center text-white pb-3">To Do List App !</h2>
+      
+        <hr  width="85%" className="bg-white mb-4" />
+
+        <Input placeholder="tech .."className="form-control mb-3" />
+
+        <TextArea placeholder='desc ..' className="form-control mb-4" rows="3" />
+
+        <Button onClick={() => console.log('clicked 🤳🏻 !')
+        } btnTitle='Add' btnStyle="btn btn-dark btn-block" />
+
+        <CardItem index={0} title='React Native' desc='Build Native Cross Platform App <IOS/Android> .. ' />
+      
+      </div>
     )
   }
 }
